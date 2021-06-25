cdca9d776b66f93dd6237ee03ee394d6c9cc7d38
FrancoTruffa <francotruffa1@gmail.com>
2021-06-25 15:29:30 -0300

    Probando Plantillas y Macros

--- dags/dag_hello_plantillasymacros.py
+++ dags/dag_hello_plantillasymacros.py
@@ -53,4 +53,9 @@ with DAG(dag_id='dag_prueba_macros',
             bash_command= 'echo {{ ds }}'
         )
 
-start >> prueba_python >> prueba_pull >> prueba_bash
+        prueba_bash_macro = BashOperator(
+            task_id = 'prueba_bash_macro',
+            bash_command= 'echo {{ ti.xcom_pull(task_ids="prueba_python") }}'
+        )
+
+start >> prueba_python >> prueba_pull >> prueba_bash >> prueba_bash_macro
